887c8d9840fe6ed3895fef325c26c6c8f9a5f90b
D2Diego <dmarques1508@gmail.com>
2024-03-22 20:25:13 -0300

    feat: layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import { Checkout } from './pages/Checkout'
 import { Success } from './pages/Success'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/sucess" element={<Success />} />
       </Route>
