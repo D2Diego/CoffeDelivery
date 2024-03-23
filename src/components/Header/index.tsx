fc39f10de97fc31ac1504c2c258cbe6f52285cb7
D2Diego <dmarques1508@gmail.com>
2024-03-22 21:07:34 -0300

    feat: iniciando as stilizações header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
+import { HeaderContainer } from './styles'
+
+import logoCoffe from '../../assets/Logo.svg'
+import { ShoppingCart, MapPin } from 'phosphor-react'
+
 export function Header() {
-  return <h1>hheader</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoCoffe} alt="logo coffe" />
+      <nav>
+        <div>
+          <MapPin size={32} />
+          Brasília, DF
+        </div>
+        <ShoppingCart size={32} />
+      </nav>
+    </HeaderContainer>
+  )
 }
