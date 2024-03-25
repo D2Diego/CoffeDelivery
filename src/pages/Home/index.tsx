025bd28ccca62aff45ca484b5d6533a9c619e358
D2Diego <dmarques1508@gmail.com>
2024-03-25 15:23:55 -0300

    fix: Header and nav home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,36 +4,47 @@ import Timer from '../../assets/Timer.svg'
 import Embalagens from '../../assets/Embalagens.svg'
 import MiniCoffe from '../../assets/MiniCoffe.svg'
 
-import { HomeContainer } from './styles'
+import { HomeContainer, Section } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
-      <div className="container-title">
-        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-        <p>
-          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-          hora
-        </p>
+      <div className="text-container">
+        <div>
+          <div className="container-title">
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </div>
+          <Section>
+            <article>
+              <img src={MiniCart} alt="carrinho de compras" />
+              Compra simples e segura
+            </article>
+            <article>
+              <img src={Timer} alt="relógio" />
+              Entrega rápida e rastreada
+            </article>
+            <article>
+              <img
+                src={Embalagens}
+                alt="embalagem"
+                className="icon-embalagens"
+              />
+              Embalagem mantém o café intacto
+            </article>
+            <article>
+              <img src={MiniCoffe} alt="café" className="icon-coffe" />O café
+              chega fresquinho até você
+            </article>
+          </Section>
+        </div>
+        <img src={CoffeImg} alt="Imagem de um café na embalagem da loja" />
       </div>
-      <section>
-        <article>
-          <img src={MiniCart} alt="" />
-          Compra simples e segura
-        </article>
-        <article>
-          <img src={Timer} alt="" />
-          Entrega rápida e rastreada
-        </article>
-        <article>
-          <img src={Embalagens} alt="" />
-          Embalagem mantém o café intacto
-        </article>
-        <article>
-          <img src={MiniCoffe} alt="" />O café chega fresquinho até você
-        </article>
-      </section>
-      <img src={CoffeImg} alt="Imagem de um café na embalagem da loja" />
+
+      <h1>Nossos cafés</h1>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,27 +1,53 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.nav`
+  .text-container {
+    display: flex;
+    flex-direction: row;
+  }
+
   .container-title h1 {
     font-family: 'Baloo 2';
     font-size: 48px;
     font-weight: 800;
+    margin-bottom: 16px;
   }
 
   .container-title p {
-    font-size: 20px;
-    font-weight: 400;
-  }
+    color: ${(props) => props.theme['base-subtitle']};
 
-  section {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 5px;
-    column-gap: 10px;
+    /* Text/Regular L */
+    font-family: Roboto;
+    font-size: 25px;
+    margin-bottom: 75px;
   }
+`
+export const Section = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 22px;
+  column-gap: 10px;
 
   article {
     display: flex;
     align-items: center;
     gap: 8px;
+
+    color: ${(props) => props.theme['base-text']};
+
+    font-family: Roboto;
+    font-size: 16px;
+  }
+
+  .icon-embalagens {
+    border-radius: 1000px;
+    background-color: ${(props) => props.theme['base-text']};
+    padding: 8px;
+  }
+
+  .icon-coffe {
+    border-radius: 1000px;
+    background-color: ${(props) => props.theme['purple-base']};
+    padding: 8px;
   }
 `
