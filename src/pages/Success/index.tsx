e8391cc3454e76969353eef8bd65aff5a12170c9
D2Diego <dmarques1508@gmail.com>
2024-03-27 18:24:03 -0300

    fix: page success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -11,32 +11,34 @@ export function Success() {
       <h1>Uhu!Pedido Confirmado</h1>
       <p>Agora é só aguardar que logo o café chegará até você</p>
       <div className="box-hero">
-        <div className="orders-info">
-          <div className="line-infos">
-            <img src={MiniLoc} alt="" />
-            <p>
-              Entrega em <strong>Rua João Daniel Martinelli</strong>,
-              102Farrapos - Porto Alegre, RS
-            </p>
-          </div>
-
-          <div className="line-infos">
-            <img src={Timer} alt="" />
-            <div className="line-text">
+        <div className="oders-info-wrapper">
+          <div className="orders-info">
+            <div className="line-infos">
+              <img src={MiniLoc} alt="" />
               <p>
-                Previsão de entrega <br />
-                <strong>20 min - 30 min </strong>
+                Entrega em <strong>Rua João Daniel Martinelli</strong>,
+                102Farrapos - Porto Alegre, RS
               </p>
             </div>
-          </div>
 
-          <div className="line-infos">
-            <img src={MiniCifrao} alt="" />
-            <div className="line-text">
-              <p>
-                Pagamento na entrega <br />
-                <strong>Cartão de Crédito</strong>
-              </p>
+            <div className="line-infos">
+              <img src={Timer} alt="" />
+              <div className="line-text">
+                <p>
+                  Previsão de entrega <br />
+                  <strong>20 min - 30 min </strong>
+                </p>
+              </div>
+            </div>
+
+            <div className="line-infos">
+              <img src={MiniCifrao} alt="" />
+              <div className="line-text">
+                <p>
+                  Pagamento na entrega <br />
+                  <strong>Cartão de Crédito</strong>
+                </p>
+              </div>
             </div>
           </div>
         </div>
